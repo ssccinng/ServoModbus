e0debf894547d56f7cfd0295cd164269c19b8d87
scixing <whs11998@163.com>
2023-04-14 14:54:24 +0800

    添加功能

--- ServoModbus/ServoClient.cs
+++ ServoModbus/ServoClient.cs
@@ -6,8 +6,9 @@ using System.IO.Ports;
 namespace ServoModbus;
 public class ServoClient
 {
-    SerialPort _serialPort { get; set; }
-    IModbusSerialMaster modbusSerialMaster { get; set; }
+
+    private SerialPort _serialPort;
+    private IModbusSerialMaster _modbusSerialMaster;
     byte slaveAddress = 0;
     public ServoClient(string ComName, byte slaveAddress = 0)
     {
@@ -17,6 +18,11 @@ public class ServoClient
         _serialPort.DataBits = 8;
         _serialPort.Parity = Parity.None;
         _serialPort.StopBits = StopBits.One;
+        
+    }
+
+    public void Connect()
+    {
         _serialPort.Open();
         _serialPort.DiscardInBuffer();
         _serialPort.DiscardOutBuffer();
@@ -24,9 +30,18 @@ public class ServoClient
         _serialPort.ReadTimeout = 500;
         var factory = new ModbusFactory(logger: new ConsoleModbusLogger(LoggingLevel.Trace));
         //factory.Logger.
-        modbusSerialMaster = factory.CreateRtuMaster(_serialPort);
+        _modbusSerialMaster = factory.CreateRtuMaster(_serialPort);
     }
+    public void DisConnect()
+    {
+        _modbusSerialMaster.Dispose();
+        _serialPort.Close();
 
+    }
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    /// <returns></returns>
     public async Task Init()
     {
         await WriteToServoAsync(0x02, 00, 1);
@@ -61,22 +76,69 @@ public class ServoClient
         //await WriteToServoAsync(0x0c, 08, 115200);
 
     }
-
-    public async Task SetDo(int idx, bool val)
+    /// <summary>
+    /// 移动结束事件
+    /// </summary>
+    public Action OnMoveEnd;
+    public async Task SetDO(int idx, bool val)
     {
 
-        await modbusSerialMaster.WriteMultipleRegistersAsync(slaveAddress, (0x31 << 8) + 0x4, new ushort[] {1});
+        await _modbusSerialMaster.WriteMultipleRegistersAsync(slaveAddress, (0x31 << 8) + 0x4, new ushort[] {1});
     }
+    /// <summary>
+    /// 设置di
+    /// </summary>
+    /// <param name="idx"></param>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public async Task SetDI(int idx, bool val)
+    {
 
-    public async Task SetEnable(bool val)
+        await _modbusSerialMaster.WriteMultipleRegistersAsync(slaveAddress, (0x31 << 8) + 0x4, new ushort[] { 1 });
+    }
+    /// <summary>
+    /// 设置使能
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public async Task SetEnableAsync(bool val)
     {
         await WriteToServoAsync(0x03, 10, new ushort[] {1});
         await WriteToServoAsync(0x03, 11, new ushort[] { (ushort)(val ? 1 : 0) });
     }
+    public async Task SetTargetAsync(byte idx, byte pos, TargetInfo targetInfo)
+    {
+        await WriteToServoAsync(11, (byte)((idx + 1) * 10), targetInfo.StartSpeed, 
+                                                targetInfo.StopSpeed, pos, targetInfo.MaxSpeed, targetInfo.MaxAccTime, 0);
+    }
+    // 让移动轴移动到指定位置
+    public async Task MoveToAsync(int target)
+    {
+        //await WriteToServoAsync(0x03, 00, new ushort[] {1});
+        //await WriteToServoAsync(0x03, 01, new ushort[] { (ushort)pos});
+         
+    }
+
+    /// <summary>
+    /// 归原
+    /// </summary>
+    /// <returns></returns>
+    public async Task ReturnZeroAsync()
+    {
+
+    }
+
+    /// <summary>
+    /// 写入数据
+    /// </summary>
+    /// <param name="h1">Hxx（16进制）</param>
+    /// <param name="h2">.xx(10进制)</param>
+    /// <param name="data">数据</param>
+    /// <returns></returns>
 
     public async Task WriteToServoAsync(byte h1, byte h2, params ushort[] data)
     {
-        await modbusSerialMaster.WriteMultipleRegistersAsync(slaveAddress, (ushort)((h1 << 8) + h2), data);
+        await _modbusSerialMaster.WriteMultipleRegistersAsync(slaveAddress, (ushort)((h1 << 8) + h2), data);
 
     }
 
---
+++ ServoModbus/TargetInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ServoModbus
+{
+    public class TargetInfo
+    {
+        public ushort StartSpeed { get; set; }
+        public ushort MaxSpeed { get; set; }
+
+        public ushort MaxAccTime { get; set; }
+        public ushort StopSpeed { get; set; } = 0;
+    }
+}
