03e2ca9332f734ceb731fc90d1e95433ce2b43b8
scixing <whs11998@163.com>
2023-04-18 18:02:16 +0800

    更新数据适配

---
+++ ServoModbus/VDITable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServoModbus;
+
+public class VDITable
+{
+    // 还需获取实际状态 通过枚举获取
+    public VDIInfo[] VDIInfo { get; set; } = new VDIInfo[64];
+}
+
+public class VDIInfo
+{
+    public DIFuncType DIFuncType { get; set; }
+    public int High { get; set; }
+}
+
+public class VDOTable
+{
+    public VDOInfo[] VDOInfo { get; set; } = new VDOInfo[64];
+}
+public class VDOInfo
+{
+    public DOFuncType DOFuncType { get; set; }
+    public int High { get; set; }
+
+}
